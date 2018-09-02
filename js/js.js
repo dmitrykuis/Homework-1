a6f28effc04c8904ef8a258270b85ba420ba8237
Dmitry Kuis <d.kuis.work@gmail.com>
2018-09-02 13:49:19 +0300

    second iteration

--- js/js.js
+++ js/js.js
@@ -1,23 +1,42 @@
-function clickedButton () {
-    window.location='pages/вторая страница.html';
+function clickedButton() {
+    window.location = 'pages/вторая страница.html';
 }
 
-function changeStyle () {
+function createElementWithClassName(tag, className) {
+    const element = document.createElement(tag);
+    if (className) {
+        element.className = className;
+    }
+
+    return element;
+}
+
+function changeStyle() {
     document.body.style.backgroundColor = "#4c7aaf";
     var red, green, yellow;
-    red = document.body.children [1];
-    red.style.cssText ="color: red; \
+    red = document.body.children[1];
+    red.style.cssText = "color: red; \
         border-color: #f44336; "
-    green = document.body.children [2];
-    green.style.cssText ="color: green; \
+    green = document.body.children[2];
+    green.style.cssText = "color: green; \
         border-color: #4CAF50; "
-    yellow = document.body.children [3];
+    yellow = document.body.children[3];
     yellow.style.cssText = "color: orange; \
     border-color: #ff9800;"
 }
 
-function deleteContent () {
+function deleteContent() {
     document.body.innerHTML = "";
-    document.body.style.backgroundColor = "#ffffff";
-    
+    renderTable();
+}
+
+function templateTable() {
+    return (`<div></div><div></div><div></div><div></div><div></div><div></div><div></div>`)
+}
+
+function renderTable(elements) {
+    const flex = createElementWithClassName('div', 'container');
+    flex.style.display = 'flex';
+    document.body.appendChild(flex);
+    flex.innerHTML = templateTable();
 }
